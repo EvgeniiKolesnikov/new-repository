c2341a66be3cb9d235254f604424258e645d9db2
EvgeniiKolesnikov <jekow@yandex.ru>
2021-06-17 16:11:12 +0300

    1st task add func slidesPlugin

--- 01-cards-sources/app.js
+++ 01-cards-sources/app.js
@@ -1,13 +1,18 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSLide = 0) {
+  const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses();
-    slide.classList.add('active');
-  })
+  slides[activeSLide-1].classList.add('active');
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+      slide.classList.add('active');
+    })
+  }
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    })
+  }
 }
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-  })
-}
+
+slidesPlugin(4);
